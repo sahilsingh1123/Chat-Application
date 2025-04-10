11f7e511c08db6db62f4408af64aef16f6ebd5c0
sahilsingh1123 <sahilsingh1123@gmail.com>
2025-04-10 06:20:29 +0530

    feat: Added support for OpenAI chat

--- bin/chat_providers.py
+++ bin/chat_providers.py
@@ -1,7 +1,12 @@
 from .cohere_ai.chat import CohereChat
+from .openai.chat import OpenAIChat
 
 class ChatFactory:
     @staticmethod
     def get_chat(chat_type):
         if chat_type == 'cohere':
             return CohereChat()
+        elif chat_type == 'openai':
+            return OpenAIChat()
+        else:
+            raise ValueError(f"Invalid chat provider: {chat_type}")
---
+++ bin/openai/chat.py
@@ -0,0 +1,44 @@
+"""
+This class contains the chat interface for
+openai API
+"""
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from ..chat_interface import Chat
+
+load_dotenv()
+API_KEY = os.getenv('OPENAI_API_KEY')
+MODEL_NAME = os.getenv('OPENAI_MODEL_NAME')
+ASSISTANT_ROLE = os.getenv('ASSISTANT_ROLE')
+
+class OpenAIChat(Chat):
+    def __init__(self):
+        super().__init__()
+        self._openai = self._get_client()
+        self._model = MODEL_NAME
+
+    def chat(self, msg):
+        res = self._openai.chat.completions.create(
+            messages=self._get_template(msg),
+            model=self._model,
+        )
+
+        return res.choices[0].message.content
+
+    def _get_template(self, msg):
+        return [
+            {
+                "role": "system",
+                "content": ASSISTANT_ROLE
+            },
+            {
+                "role": "user",
+                "content": msg
+            },
+        ]
+
+    def _get_client(self):
+        return OpenAI(
+            api_key=API_KEY,
+        )
